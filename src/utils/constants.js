a67f4258f69ccd35f69f9840ab9599017fc67647
Mohit Kumar <mohit3312@gmail.com>
2026-02-05 16:22:41 +0530

    Refactor: Moved API key in .env file and added an entry for .env file in .gitignore file.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const GOOGLE_API_KEY = "AIzaSyD37Kjg3yJikl_CMGTg61GvUm9M5o4-KHQ";
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY;
+  API_KEY;
