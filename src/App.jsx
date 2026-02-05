a70b9ef536815d986c46fed73822098135b1bb1a
Mohit Kumar <mohit3312@gmail.com>
2026-02-05 11:48:54 +0530

    Revert "feat: integrate Redux for state management and implement routing"
    This reverts commit 88021cd18ec48aba8a7c4afee29e3cefdc48af03.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,35 @@
-import { Provider } from "react-redux";
-import Body from "./components/Body";
-import Head from "./components/Head";
-import store from "./utils/store";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import MainContainer from "./components/MainContainer";
-import WatchPage from "./components/WatchPage";
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Body />,
-    children: [
-      {
-        path: "/",
-        element: <MainContainer />,
-      },
-      {
-        path: "/watch",
-        element: <WatchPage />,
-      },
-    ],
-  },
-]);
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <Provider store={store}>
-      <div>
-        <Head />
-        <RouterProvider router={appRouter} />
+  const [count, setCount] = useState(0)
 
-        {/**
-         * Head
-         * Body
-         *   Sidebar
-         *     MenuItems
-         *   MainContainer
-         *     ButtonsList
-         *     VideoContainer
-         *       Video
-         */}
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </Provider>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-const GOOGLE_API_KEY = "AIzaSyD37Kjg3yJikl_CMGTg61GvUm9M5o4-KHQ";
-
-export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY;
